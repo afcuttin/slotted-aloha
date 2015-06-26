8a5a9e24a8fd3885e5934d48dad0e0e85847579a
afcuttin <afcuttin@gmail.com>
2015-06-26 18:59:35 +0200

    path loss model variables added

--- packetCapture.m
+++ packetCapture.m
@@ -5,12 +5,18 @@ function [capturedSource] =  packetCapture(sourceStatus,sourcePower,sourceRho,ca
 % Returns 0 if no capture occurs.
 % If capture occurs, returns the index of sourceStatus corresponding to the source whose packet has been captured
 
+% //TODO add input parameters check
+
+% these parameters can be exposed as function parameters
+pathLossAlpha = 4; % 2 correspond to free space path loss, 4 to cellular environment
+pathLossModel = 1; % 1 for Bounded Path Loss Model, 0 for Unbouded Path Loss Model
+
 if numel(find(sourceStatus == 1)) == 1
 	capturedSource = find(sourceStatus == 1)
 	fprintf('Warning: there are no collisions!\nNevertheless, I provide you with the right answer without computing the capture ratio.\n')
 elseif numel(find(sourceStatus == 1)) > 1
 	collided = find(sourceStatus == 1)
-	receivedPower = sourcePower(collided)./(1+sourceRho(collided).^2)
+	receivedPower = sourcePower(collided)./(pathLossModel+sourceRho(collided).^pathLossAlpha)
 	captured = collided(find(receivedPower == max(receivedPower)))
 	interfering = setdiff(collided,captured)
 	captureRatio = sum(receivedPower.*ismember(collided,captured)) / sum(receivedPower.*not(ismember(collided,captured)))
