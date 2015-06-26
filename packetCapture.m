4e263ab2fdfd67ae51117b72f3166e3e93d2dcd0
afcuttin <afcuttin@gmail.com>
2015-06-26 13:26:26 +0200

    Added conditional control in case of no collisions

--- packetCapture.m
+++ packetCapture.m
@@ -5,15 +5,20 @@ function [capturedSource] =  packetCapture(sourceStatus,sourcePower,sourceRho,ca
 % Returns 0 if no capture occurs.
 % If capture occurs, returns the index of sourceStatus corresponding to the source whose packet has been captured
 
-collided = find(sourceStatus == 1)
-receivedPower = sourcePower(collided)./(1+sourceRho(collided).^2)
-captured = collided(find(receivedPower == max(receivedPower)))
-interfering = setdiff(collided,captured)
-captureRatio = sum(receivedPower.*ismember(collided,captured)) / sum(receivedPower.*not(ismember(collided,captured)))
-captureRatiodB = 10 * log10(captureRatio)
-if captureRatiodB >= captureThreshold
-	capturedSource = captured;
-elseif captureRatiodB < captureThreshold
-	capturedSource = 0;
+if numel(find(sourceStatus == 1)) == 1
+	capturedSource = find(sourceStatus == 1)
+	fprintf('Warning: there are no collisions!\nNevertheless, I provide you with the right answer without computing the capture ratio.\n')
+elseif numel(find(sourceStatus == 1)) > 1
+	collided = find(sourceStatus == 1)
+	receivedPower = sourcePower(collided)./(1+sourceRho(collided).^2)
+	captured = collided(find(receivedPower == max(receivedPower)))
+	interfering = setdiff(collided,captured)
+	captureRatio = sum(receivedPower.*ismember(collided,captured)) / sum(receivedPower.*not(ismember(collided,captured)))
+	captureRatiodB = 10 * log10(captureRatio)
+	if captureRatiodB >= captureThreshold
+		capturedSource = captured;
+	elseif captureRatiodB < captureThreshold
+		capturedSource = 0;
+	end
 end
 
